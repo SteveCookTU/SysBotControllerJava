7e4f0d8446195550283f4e95c379a267965c44eb
Steve Cook <steviecook210@gmail.com>
2021-12-30 01:38:05 -0500

    Change to completable future

--- src/main/java/me/ezpzstreamz/sysbotcontroller/SysBotController.java
+++ src/main/java/me/ezpzstreamz/sysbotcontroller/SysBotController.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 public class SysBotController {
 
@@ -22,7 +26,7 @@ public class SysBotController {
     public void connect() {
         try {
             conn = new Socket(ip, port);
-            if(conn.isConnected()) {
+            if (conn.isConnected()) {
                 System.out.println("Connected");
             }
             out = new PrintWriter(conn.getOutputStream(), true);
@@ -44,104 +48,174 @@ public class SysBotController {
 
     public String parseJumps(String pointer) {
         String ptr = pointer;
-        if(ptr.charAt(ptr.length() - 1) == ']')
+        if (ptr.charAt(ptr.length() - 1) == ']')
             ptr += "+0";
         String[] eval = ptr.replace("main", "").replace("[", "").replace("]", "").split("\\+");
         StringBuilder jumps = new StringBuilder();
         for (String s :
                 eval) {
-            if(!s.equalsIgnoreCase(""))
+            if (!s.equalsIgnoreCase(""))
                 jumps.append(" 0x").append(s);
         }
         return jumps.toString().trim();
     }
 
-    public String resolvePointer(String ptr) throws InterruptedException, IOException {
-        sendCommand("pointerRelative " + parseJumps(ptr));
-        Thread.sleep(500);
-        return in.readLine();
-    }
-
-    public void sendCommand(String command) {
-        System.out.println(command);
-        out.println(command);
-    }
-
-    public void click(String button, int sleep) throws InterruptedException {
-        sendCommand("click " + button);
-        Thread.sleep(sleep);
-    }
-
-    public void press(String button, int sleep) throws InterruptedException {
-        sendCommand("press " + button);
-        Thread.sleep(sleep);
-    }
-
-    public void release(String button, int sleep) throws InterruptedException {
-        sendCommand("release " + button);
-        Thread.sleep(sleep);
-    }
-
-    public String peek(String ptr, int size) throws InterruptedException, IOException {
-        sendCommand("pointerPeek " + size + " " + parseJumps(ptr));
-        Thread.sleep(400);
-        return in.readLine();
-    }
-
-    public void poke(String ptr, String data) {
-        sendCommand("pointerPoke " + data + " " + parseJumps(ptr));
-    }
-
-    public void setStick(String stick, int x, int y, int sleep) throws InterruptedException {
-        sendCommand("setStick " + stick + " " + x + " " + y);
-        Thread.sleep(sleep);
-    }
-
-    public String getTitleID() throws InterruptedException, IOException {
-        sendCommand("getTitleID");
-        Thread.sleep(400);
-        return in.readLine();
-    }
-
-    public String getHeapBase() throws InterruptedException, IOException {
-        sendCommand("getHeapBase");
-        Thread.sleep(400);
-        return in.readLine();
-    }
-
-    public String getMainNsoBase() throws InterruptedException, IOException {
-        sendCommand("getMainNsoBase");
-        Thread.sleep(400);
-        return in.readLine();
-    }
-
-    public void enterCode(String code) {
-        StringBuilder command = new StringBuilder();
-        for(byte b : code.getBytes()) {
-            if(b == '0') {
-                command.append(39).append(" ");
-            } else {
-                command.append(b - 19).append(" ");
+    public CompletableFuture<String> resolvePointer(String ptr) {
+        return sendCommand("pointerRelative " + parseJumps(ptr)).thenCompose(v -> CompletableFuture.supplyAsync(() -> {
+            try {
+                String line = in.readLine();
+                while (line == null) {
+                    line = in.readLine();
+                }
+                return line;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-        sendCommand("key " + command.toString().trim());
+            return null;
+        }));
     }
 
-    public void touch(int x, int y) {
-        sendCommand("touch " + x + " " + y);
+    public CompletableFuture<Void> sendCommand(String command) {
+        return CompletableFuture.runAsync(() -> {
+            System.out.println(command);
+            out.println(command);
+        });
     }
 
-    public void updateFreezeRate() {
-        sendCommand("configure freeRate 1");
+    public CompletableFuture<Void> click(String button, int sleep) {
+        return sendCommand("click " + button).thenRun(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    public void freeze(String ptr, String data) throws IOException, InterruptedException {
-        sendCommand("freeze 0x" + resolvePointer(ptr).trim() + " " + data);
+    public CompletableFuture<Void> press(String button, int sleep) {
+        return sendCommand("press " + button).thenRun(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    public void moveForward(int sleep) throws InterruptedException {
-        setStick("LEFT", 0, 0x7FFE, sleep);
-        setStick("LEFT", 0, 0, 0);
+    public CompletableFuture<Void> release(String button, int sleep) {
+        return sendCommand("release " + button).thenRun(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public CompletableFuture<String> peek(String ptr, int size) {
+        return sendCommand("pointerPeek " + size + " " + parseJumps(ptr)).thenCompose(
+                v -> CompletableFuture.supplyAsync(() -> {
+                    try {
+                        String line = in.readLine();
+                        while (line == null) {
+                            line = in.readLine();
+                        }
+                        return line;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }));
+    }
+
+    public CompletableFuture<Void> poke(String ptr, String data) {
+        return sendCommand("pointerPoke " + data + " " + parseJumps(ptr));
+    }
+
+    public CompletableFuture<Void> setStick(String stick, int x, int y, int sleep) {
+        return sendCommand("setStick " + stick + " " + x + " " + y).thenRun(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public CompletableFuture<String> getTitleID() {
+        return sendCommand("getTitleID").thenCompose(v -> CompletableFuture.supplyAsync(() -> {
+            try {
+                String line = in.readLine();
+                while (line == null) {
+                    line = in.readLine();
+                }
+                return line;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }));
+    }
+
+    public CompletableFuture<String> getHeapBase() {
+        return sendCommand("getHeapBase").thenCompose(v -> CompletableFuture.supplyAsync(() -> {
+            try {
+                String line = in.readLine();
+                while (line == null) {
+                    line = in.readLine();
+                }
+                return line;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }));
+    }
+
+    public CompletableFuture<String> getMainNsoBase() {
+        return sendCommand("getMainNsoBase").thenCompose(v -> CompletableFuture.supplyAsync(() -> {
+            try {
+                String line = in.readLine();
+                while (line == null) {
+                    line = in.readLine();
+                }
+                return line;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }));
+    }
+
+    public CompletableFuture<Void> enterCode(String code) {
+        return CompletableFuture.supplyAsync(() -> {
+            StringBuilder command = new StringBuilder();
+            for (byte b : code.getBytes()) {
+                if (b == '0') {
+                    command.append(39).append(" ");
+                } else {
+                    command.append(b - 19).append(" ");
+                }
+            }
+            return command.toString();
+        }).thenCompose(s -> sendCommand("key " + s.trim()));
+    }
+
+    public CompletableFuture<Void> touch(int x, int y) {
+        return sendCommand("touch " + x + " " + y);
+    }
+
+    public CompletableFuture<Void> updateFreezeRate() {
+        return sendCommand("configure freeRate 1");
+    }
+
+    public CompletableFuture<Void> freeze(String ptr, String data) throws ExecutionException, InterruptedException {
+        return sendCommand("freeze 0x" + resolvePointer(ptr).get().trim() + " " + data);
+    }
+
+    public CompletableFuture<Void> moveForward(int sleep) {
+        return setStick("LEFT", 0, 0x7FFE, sleep).thenCompose(v -> setStick("LEFT", 0, 0, 0));
+
     }
 
 }
